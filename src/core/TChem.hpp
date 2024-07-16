ac89ac91df3e562d338e3b69f7b76692b10b1e24
Oscar Diaz-Ibarra <73365179+odiazib@users.noreply.github.com>
2024-07-16 15:17:42 -0600

    tchem- Updating header of source code. (#38)
    * tchem- Updating header of source code.
    
    * tchem - license.

--- src/core/TChem.hpp
+++ src/core/TChem.hpp
@@ -1,22 +1,22 @@
 /* =====================================================================================
-TChem version 2.0
-Copyright (2020) NTESS
-https://github.com/sandialabs/TChem
+TChem-atm version 1.0
+Copyright (2024) NTESS
+https://github.com/sandialabs/TChem-atm
 
-Copyright 2020 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+Copyright 2024 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
 Under the terms of Contract DE-NA0003525 with NTESS, the U.S. Government retains
 certain rights in this software.
 
-This file is part of TChem. TChem is open source software: you can redistribute it
+This file is part of TChem-atm. TChem-atm is open source software: you can redistribute it
 and/or modify it under the terms of BSD 2-Clause License
 (https://opensource.org/licenses/BSD-2-Clause). A copy of the licese is also
 provided under the main directory
 
-Questions? Contact Cosmin Safta at <csafta@sandia.gov>, or
-           Kyungjoo Kim at <kyukim@sandia.gov>, or
-           Oscar Diaz-Ibarra at <odiazib@sandia.gov>
+Questions? Contact Oscar Diaz-Ibarra at <odiazib@sandia.gov>, or
+           Mike Schmidt at <mjschm@sandia.gov>, or
+           Cosmin Safta at <csafta@sandia.gov>
 
-Sandia National Laboratories, Livermore, CA, USA
+Sandia National Laboratories, New Mexico/Livermore, NM/CA, USA
 ===================================================================================== */
 #ifndef __TCHEM_HPP__
 #define __TCHEM_HPP__

285ecf96bee025c162d2ee1e549eba762d4ac6e2
Oscar <odiazib@sandia.gov>
2024-08-12 16:05:50 -0600

    Unit test for Kokkos-kernels ODE solver.

--- src/unit-tests/TChem_Test_KokkosKernelsODE.cpp
+++ src/unit-tests/TChem_Test_KokkosKernelsODE.cpp
@@ -19,7 +19,44 @@ Questions? Contact Oscar Diaz-Ibarra at <odiazib@sandia.gov>, or
 Sandia National Laboratories, New Mexico/Livermore, NM/CA, USA
 ===================================================================================== */
 #include <gtest/gtest.h>
-#include "TChem_Test_KokkosKernelsODE_Host.hpp"
+#include "TChem.hpp"
+int TestExamplesInternal(std::string path_inputs, std::string exec) {
+  {
+    int r_val(0);
+    std::string logfile(exec + ".test-log");
+    std::string rm=("rm -f " + logfile);
+    const auto rm_c_str = rm.c_str();
+    r_val = std::system(rm_c_str); 
+    TCHEM_CHECK_ERROR(r_val, "system call rm -f returns non-zero return value");
+
+    std::string chemFile=" --chemfile="+path_inputs+"config_gas.yaml ";
+    std::string aerofile="--aerofile="+path_inputs+"test_SIMPOL_phase_transfer.yaml ";
+    std::string inputfile_particles="--inputfile_particles="+path_inputs+"scenario_conditions_particle.yaml ";
+    std::string outputfile="--outputfile=full_gas_KK.dat ";
+    std::string dtmin="--dtmin=10 ";
+    std::string tend="--tend=50 "; 
+    
+    std::string invoke=(exec + chemFile + aerofile +
+                        inputfile_particles + outputfile + dtmin + tend + " > " + logfile);
+    const auto invoke_c_str = invoke.c_str();
+    printf("Tines testing : %s\n", invoke_c_str);
+    r_val = std::system(invoke_c_str);
+    TCHEM_CHECK_ERROR(r_val, "system call example executable returns non-zero return value");
+    std::ifstream file(logfile);
+    for (std::string line; getline(file, line); ) {
+      printf("%s\n", line.c_str());
+      EXPECT_TRUE(line.find("FAIL") == std::string::npos);
+    }
+  }
+  return 0;
+}
+
+TEST(TimeIntegration,KokkosKernelsBDF) {
+  TestExamplesInternal(
+    "../examples/runs/atmospheric_chemistry/Simpol/",
+    "../examples/TChem_AerosolChemistry_KokkosKernels.x");
+}
+
 int
 main(int argc, char* argv[])
 {

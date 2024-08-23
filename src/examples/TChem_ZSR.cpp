ef162c9aad8bd1bcd01bfb279e177ce075eb4a79
Frederick <sf20@keeling.earth.illinois.edu>
2024-08-23 10:49:46 -0500

    Swap out use of conventional state array with 1D view consistent with other references to TChem's state variable.

--- src/examples/TChem_ZSR.cpp
+++ src/examples/TChem_ZSR.cpp
@@ -40,33 +40,75 @@ main(int argc, char* argv[])
 
     using aerosol_water_single_particle_type = TChem::Impl::AerosolWater_SingleParticle<real_type, host_device_type>;
     
+    // create a 1d view number_conc with length equal to the number of computational particles
+    // (set via YAML AERO_REP_SINGLE_PARTICLE entry "maximum computational particles"), currently set = 1
+    real_type_1d_view number_conc("number_conc", amcd.nParticles);
+
+    // assuming constant number concentration
+    // set each computational particle to have multiplicity 1.3e6
+    Kokkos::deep_copy(number_conc, 1.3e6); // particle number concentration
+
     // Just run serial right now for simplicity
     const auto member = Tines::HostSerialTeamMember();
 
-    // TODO: use TChem's state data structure
-    int n_particles = 1;
-    real_type state[amd.aerosol_sp_name_idx_.size()+1]; // add 1 for gas phase water
-    int rh_index = amd.aerosol_sp_name_idx_.size();
+    ordinal_type ntotal_species = amcd.nSpec_gas + amcd.nSpec*amcd.nParticles;
+
+    //printf("[TChem_ZSR::main] Number of gas species: %d\n", amcd.nSpec_gas);
+    //printf("[TChem_ZSR::main] Number of aerosol species: %d\n", amcd.nSpec);
+    // TChem's state data structure
+    real_type_1d_view state("state", ntotal_species);
+
+    //real_type state[amd.aerosol_sp_name_idx_.size()+1]; // add 1 for gas phase water
+    //printf("[TChem_ZSR::main] Number of aerosol species according to aerosol_sp_name_idx_: %d\n", amd.aerosol_sp_name_idx_.size());
+    //int rh_index = amd.aerosol_sp_name_idx_.size();
     int i_part = 0;
-    int number_conc = 1.0;
+    //int number_conc = 1.0;
 
     
-    ordinal_type rh_idx = amd.aerosol_sp_name_idx_.size();
+    //ordinal_type rh_idx = amd.aerosol_sp_name_idx_.size();
+
+    // relative index of H2O_aq amongst aerosol species (not amongst both gas and aerosol + multiple particles)
     ordinal_type aqueous_water_idx = amd.aerosol_sp_name_idx_.at("H2O_aq");
 
+
+    //printf("[TChem_ZSR::main] index of H2O: %d\n", rh_idx);
+    //printf("[TChem_ZSR::main] index of H2O_aq: %d\n", aqueous_water_idx);
+    //printf("[TChem_ZSR::main] index of Cl_m: %d\n", amd.aerosol_sp_name_idx_.at("Cl_m"));
+    //printf("[TChem_ZSR::main] index of Ca_pp: %d\n", amd.aerosol_sp_name_idx_.at("Ca_pp"));
+    //printf("[TChem_ZSR::main] index of Na_p: %d\n", amd.aerosol_sp_name_idx_.at("Na_p"));
+            
     // Loop over RH
     for (int i; i<101; i++){
 
+        // gas species
+        ordinal_type rh_idx = 0;
+        state(rh_idx) = i/100.0; //  H2O (gas phase)
+
+        // aerosol species
+        for (int i_part = 0; i_part < amcd.nParticles; i_part++){
+            state(1+amcd.nSpec*i_part) = 2.5; // Na_p
+            state(2+amcd.nSpec*i_part) = 5.3; // Cl_m
+            state(3+amcd.nSpec*i_part) = 1.3; // Ca_pp
+            state(4+amcd.nSpec*i_part) = 0.0; // H2O_aq
+        }// i_part
+       member.team_barrier();
+
+        /*
         state[amd.aerosol_sp_name_idx_.at("Cl_m")] = 5.3;    // Cl
         state[amd.aerosol_sp_name_idx_.at("Ca_pp")] = 1.3;   // Ca
-        state[rh_index] = i/100.0; // H2O (gas phase) (note gas phase H2O is not part of the species mapping)
+        state[rh_idx] = i/100.0; // H2O (gas phase) (note gas phase H2O is not part of the species mapping)
         state[amd.aerosol_sp_name_idx_.at("H2O_aq")] = 0;    // H2O (aerosol phase)
-        
+        */ 
+
+        // TODO use parallel for over particles
+        int i_part = 0;
         aerosol_water_single_particle_type::team_invoke(member, i_part, number_conc, state, amcd, rh_idx, aqueous_water_idx);
         //printf("[TChem_ZSR::main] RH %f\n", state[2]);
         //printf("[TChem_ZSR::main] Total aerosol water content %f\n\n", state[3]);
-        printf("%f,%f\n", state[rh_index], state[amd.aerosol_sp_name_idx_.at("H2O_aq")]);
+        printf("%f,%f\n", state(rh_idx), state(amcd.nSpec_gas + amcd.nSpec*i_part + aqueous_water_idx));
+      
     }
+    
 
     
         

b91c0c749d95cbcd24ee87ec2f6e749cb15b02d0
Oscar Diaz-Ibarra <odiazib@sandia.gov>
2024-09-12 16:24:31 -0600

    Remove files.

--- aerosol_performance/loadMachinePathsTemplate.sh
+++
@@ -1,6 +0,0 @@
-
-export KOKKOS_TOOLS_LIBS=/path/kokkostools/libs/libkp_kernel_timer.so
-TCHEM_INSTALL_PATH=/path/tchem/install
-exec=${TCHEM_INSTALL_PATH}/examples/${experiment_exe_root}.x
-machine_name=/name/machine
-

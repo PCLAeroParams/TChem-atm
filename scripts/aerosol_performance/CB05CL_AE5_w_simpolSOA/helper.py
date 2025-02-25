73391f236c3e64f3d560899364e4d95e9a37c62b
Sam Frederick <samfred12@gmail.com>
2025-02-25 09:49:37 -0800

    Improve exception handling by passing exception to print statement

--- scripts/aerosol_performance/CB05CL_AE5_w_simpolSOA/helper.py
+++ scripts/aerosol_performance/CB05CL_AE5_w_simpolSOA/helper.py
@@ -36,29 +36,31 @@ def create_dataFrame_gpu(flist):
     # fileNames.append(fname)
     try:
       data = pd.read_json(file)
-      colName = np.array(fname.replace('.json', '').split('_'))
-      # print(colName)
-      nBatch = colName[3]
-      # note: we have this set to 1 currently, but keeping in case of change
-      vector_size = colName[5]
-      team_size = colName[8]
-      nParticles =colName[12]
-      # colName = '_' + nParticles +'p'
-      colName = '_' + nBatch+'t_'+ team_size  +'v_'+ vector_size  + 'b_'  + nParticles +'p'
+    except ValueError as e:
+        print(f'Unable to read {file}: {e}')
+        continue
+    colName = np.array(fname.replace('.json', '').split('_'))
+    # print(colName)
+    nBatch = colName[3]
+    # note: we have this set to 1 currently, but keeping in case of change
+    vector_size = colName[5]
+    team_size = colName[8]
+    nParticles =colName[12]
+    # colName = '_' + nParticles +'p'
+    colName = '_' + nBatch+'t_'+ team_size  +'v_'+ vector_size  + 'b_'  + nParticles +'p'
 
-      data.rename(columns={'Atmospheric Chemistry E3SM': 'walltime'}, inplace=True)
-      data.rename(lambda x: x.replace('wall_time_', ''), axis='index', inplace=True)
-      data.rename({'number_of_samples': 'nSamples', 'number_of_time_iters': 'nIter',
-                 'iter_0': 'walltime'},
-                  axis='index', inplace=True)
-      data.loc['nBatch'] = int(nBatch)
-      data.loc['vector_size'] = int(vector_size)
-      data.loc['team_size'] = int(team_size)
-      data.loc['nParticles'] = int(nParticles)
-      tmp = data.add_suffix(colName, axis=1)
-      df_list.append(tmp)
-    except:
-      print(file + "has issues.")
+    data.rename(columns={'Atmospheric Chemistry E3SM': 'walltime'}, inplace=True)
+    data.rename(lambda x: x.replace('wall_time_', ''), axis='index', inplace=True)
+    data.rename({'number_of_samples': 'nSamples', 'number_of_time_iters': 'nIter',
+                'iter_0': 'walltime'},
+                axis='index', inplace=True)
+    data.loc['nBatch'] = int(nBatch)
+    data.loc['vector_size'] = int(vector_size)
+    data.loc['team_size'] = int(team_size)
+    data.loc['nParticles'] = int(nParticles)
+    tmp = data.add_suffix(colName, axis=1)
+    df_list.append(tmp)
+
   df = pd.concat(df_list, axis=1)
   df.sort_values(by=['nBatch', 'vector_size','team_size','nParticles'], axis=1, inplace=True)
   df = df.transpose()

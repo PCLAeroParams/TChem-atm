90147f910e820438364889bb10ebc33457d6022a
Jeff Curtis <jcurtis2@illinois.edu>
2025-05-19 14:10:34 -0500

    clean up unused code. move cvode stats to function

--- src/core/TChem_Driver.cpp
+++ src/core/TChem_Driver.cpp
@@ -24,6 +24,41 @@ static int check_flag(const int flag, const std::string funcname)
   return 0;
 }
 
+void print_cvode_statistics(void* cvode_mem) {
+    long int nst, nfe, nsetups, nje, nni, ncfn, netf;
+    int retval;
+
+    retval = CVodeGetNumSteps(cvode_mem, &nst);
+    check_flag(retval, "CVodeGetNumSteps");
+
+    retval = CVodeGetNumRhsEvals(cvode_mem, &nfe);
+    check_flag(retval, "CVodeGetNumRhsEvals");
+
+    retval = CVodeGetNumLinSolvSetups(cvode_mem, &nsetups);
+    check_flag(retval, "CVodeGetNumLinSolvSetups");
+
+    retval = CVodeGetNumErrTestFails(cvode_mem, &netf);
+    check_flag(retval, "CVodeGetNumErrTestFails");
+
+    retval = CVodeGetNumNonlinSolvIters(cvode_mem, &nni);
+    check_flag(retval, "CVodeGetNumNonlinSolvIters");
+
+    retval = CVodeGetNumNonlinSolvConvFails(cvode_mem, &ncfn);
+    check_flag(retval, "CVodeGetNumNonlinSolvConvFails");
+
+    retval = CVodeGetNumJacEvals(cvode_mem, &nje);
+    check_flag(retval, "CVodeGetNumJacEvals");
+
+    std::cout << "\nFinal Statistics:\n"
+              << "  Steps            = " << nst << "\n"
+              << "  RHS evals        = " << nfe << "\n"
+              << "  LS setups        = " << nsetups << "\n"
+              << "  Jac evals        = " << nje << "\n"
+              << "  NLS iters        = " << nni << "\n"
+              << "  NLS fails        = " << ncfn << "\n"
+              << "  Error test fails = " << netf << "\n";
+}
+
 /* Initialize the model given input YAML files */
 void initialize(const char* chemFile, const char* aeroFile, const char* numericsFile,
   const ordinal_type nBatch){
@@ -31,7 +66,6 @@ void initialize(const char* chemFile, const char* aeroFile, const char* numerics
   g_tchem = new TChem::Driver();
 
   Kokkos::InitializationSettings settings;
-//  settings.set_num_threads(8);
   settings.set_device_id(0);
   Kokkos::initialize(settings);
 
@@ -315,116 +349,9 @@ ordinal_type TChem::Driver::getNumberConcentrationVectorSize() const{
 
 /* Integrate a time step */
 void TChem_doTimestep(const double &del_t){
-//  g_tchem->doTimestep(del_t);
-  g_tchem->doTimestep_sparse(del_t);
+  g_tchem->doTimestep(del_t);
 }
 
-/* Integrate a time step */
-/*
-void TChem::Driver::doTimestep(const double del_t){
-
-  const auto exec_space_instance = TChem::exec_space();
-  using device_type = typename Tines::UseThisDevice<TChem::exec_space>::type;
-  using interf_host_device_type = typename Tines::UseThisDevice<TChem::host_exec_space>::type;
-  using problem_type = TChem::Impl::AerosolChemistry_Problem<real_type, interf_host_device_type>;
-  using policy_type = typename TChem::UseThisTeamPolicy<TChem::exec_space>::type;
-
-  policy_type policy(exec_space_instance, _nBatch, Kokkos::AUTO());
-
-  if (_team_size > 0 && _vector_size > 0) {
-      policy = policy_type(exec_space_instance,  _nBatch, _team_size, _vector_size);
-  } else if (_team_size > 0 && _vector_size < 0) {
-      policy = policy_type(exec_space_instance, _nBatch,  _team_size);
-  }
-
-  using time_integrator_cvode_type = Tines::TimeIntegratorCVODE<real_type,host_device_type>;
-  Tines::value_type_1d_view<time_integrator_cvode_type,interf_host_device_type> cvodes;
-  cvodes = Tines::value_type_1d_view<time_integrator_cvode_type,interf_host_device_type>("cvodes", _nBatch);
-
-  const ordinal_type total_n_species = _kmcd_host.nSpec + _amcd_host.nSpec * _amcd_host.nParticles;
-  const ordinal_type n_active_vars = total_n_species - _kmcd_host.nConstSpec;
-  for (ordinal_type i=0;i<_nBatch;++i){
-       cvodes(i).create(n_active_vars);
-  }
-
-  const ordinal_type level = 1;
-  ordinal_type per_team_extent(0);
-
- // per_team_extent = TChem::AtmosphericChemistry::getWorkSpaceSize(_kmcd_device);
-  per_team_extent = TChem::AerosolChemistry_CVODE::getWorkSpaceSize(_kmcd_host, _amcd_host);
-
-  const ordinal_type per_team_scratch =
-      TChem::Scratch<real_type_1d_view>::shmem_size(per_team_extent);
-  policy.set_scratch_size(level, Kokkos::PerTeam(per_team_scratch));
-
- // auto number_of_equations = problem_type::getNumberOfTimeODEs(_kmcd_host);
-  auto number_of_equations = problem_type::getNumberOfTimeODEs(_kmcd_host, _amcd_host);
-  real_type_2d_view tol_time("tol time", number_of_equations, 2);
-  real_type_1d_view tol_newton("tol newton", 2);
-  real_type_2d_view fac("fac", _nBatch, number_of_equations);
-
-  auto tol_time_host = Kokkos::create_mirror_view(tol_time);
-  auto tol_newton_host = Kokkos::create_mirror_view(tol_newton);
-  for (ordinal_type i = 0, iend = tol_time.extent(0); i < iend; ++i) {
-      tol_time_host(i, 0) = _atol_time;
-      tol_time_host(i, 1) = _rtol_time;
-  }
-  tol_newton_host(0) = _atol_newton;
-  tol_newton_host(1) = _rtol_newton;
-
-  Kokkos::deep_copy(tol_time, tol_time_host);
-  Kokkos::deep_copy(tol_newton, tol_newton_host);
-
-
-  using time_advance_type = TChem::time_advance_type;
-  time_advance_type tadv_default;
-  tadv_default._tbeg = 0.0;
-  tadv_default._tend = del_t;
-  tadv_default._dt = _dtmin;
-  tadv_default._dtmin = _dtmin;
-  tadv_default._dtmax = del_t;
-  tadv_default._max_num_newton_iterations = _max_num_newton_iterations;
-  tadv_default._num_time_iterations_per_interval = _num_time_iterations_per_interval;
-  tadv_default._jacobian_interval = _jacobian_interval;
-
-  time_advance_type_1d_view tadv("tadv", _nBatch);
-  Kokkos::deep_copy(tadv, tadv_default);
-  real_type_1d_view t("time", _nBatch);
-  Kokkos::deep_copy(t, tadv_default._tbeg);
-  real_type_1d_view dt("delta time", _nBatch);
-  Kokkos::deep_copy(dt, tadv_default._dt);
-
-  real_type tend = del_t;
-  ordinal_type iter = 0;
-  real_type tsum(0);
-  auto stateVecDim = TChem_getLengthOfStateVector();
-  real_type_2d_view state("StateVector Devices", _nBatch, stateVecDim);
-  Kokkos::deep_copy(state, _state);
-  real_type_2d_view number_conc("NumberConcentration", _nBatch, _amcd_host.nParticles);
-  Kokkos::deep_copy(number_conc, _number_concentration);
-
-  const real_type zero(0);
-
-  for (; iter < _max_num_time_iterations && tsum <= tend * 0.9999; ++iter) {
-       TChem::AerosolChemistry_CVODE::runHostBatch(
-              policy, tol_time, fac, tadv, state, number_conc, t, dt, state,
-              _kmcd_host, _amcd_host, cvodes);
-
-    tsum = zero;
-
-    Kokkos::parallel_reduce(
-        Kokkos::RangePolicy<TChem::exec_space>(0, _nBatch),
-        KOKKOS_LAMBDA(const ordinal_type &i, real_type &update) {
-          tadv(i)._tbeg = t(i);
-          tadv(i)._dt = dt(i);
-          update += t(i);
-     }, tsum);
-     Kokkos::fence();
-  }
-  Kokkos::deep_copy(_state, state);
-
-}
-*/
 /* Return the state vector from host */
 void TChem::Driver::getStateVectorHost(real_type_2d_const_view_host &view) {
   TCHEM_CHECK_ERROR(_state.span() == 0, "State vector should be constructed");
@@ -432,7 +359,7 @@ void TChem::Driver::getStateVectorHost(real_type_2d_const_view_host &view) {
 }
 
 /* Integrate a time step */
-void TChem::Driver::doTimestep_sparse(const double del_t){
+void TChem::Driver::doTimestep(const double del_t){
 
   using VecType   = sundials::kokkos::Vector<TChem::exec_space>;
   using MatType   = sundials::kokkos::DenseMatrix<TChem::exec_space>;
@@ -465,17 +392,13 @@ void TChem::Driver::doTimestep_sparse(const double del_t){
   udata.nbatches = _nBatch;
   real_type_2d_view number_conc("NumberConcentration", _nBatch, _amcd_host.nParticles);
   Kokkos::deep_copy(number_conc, _number_concentration);
-  udata.num_concentration = number_conc; //_number_concentration;
+  udata.num_concentration = number_conc;
   udata.batchSize=number_of_equations;
-  udata.kmcd=_kmcd_device; //host;
-  udata.amcd=_amcd_device; //host;
+  udata.kmcd=_kmcd_device;
+  udata.amcd=_amcd_device;
 
   // Create the SUNDIALS context
   sundials::Context sunctx;
-/*  using real_type_1d_view_type = Tines::value_type_1d_view<real_type, device_type>;
-  using real_type_2d_view_type = Tines::value_type_2d_view<real_type, device_type>;
-  using real_type_2d_view_host_type = Tines::value_type_2d_view<real_type, host_device_type>;
-*/
   // Create vector with the initial condition
   const sunrealtype T0 = SUN_RCONST(0.0);
 
@@ -567,7 +490,7 @@ void TChem::Driver::doTimestep_sparse(const double del_t){
 
 //    if (check_flag(retval, "CVodeSetLinearSolver")) { return 1; }
     per_team_extent
-         = TChem::Impl::Aerosol_RHS<real_type, device_type>::getWorkSpaceSize(_kmcd_device, _amcd_device); //host, _amcd_host);
+         = TChem::Impl::Aerosol_RHS<real_type, device_type>::getWorkSpaceSize(_kmcd_device, _amcd_device);
 
     const ordinal_type per_team_scratch =
       TChem::Scratch<real_type_1d_view_type>::shmem_size(per_team_extent);
@@ -575,9 +498,8 @@ void TChem::Driver::doTimestep_sparse(const double del_t){
     udata.policy = policy;
 
     // Final time and time between outputs
-
     const sunrealtype Tf    = SUN_RCONST(del_t);
-    const sunrealtype dTout = SUN_RCONST(del_t); //_dtmin);
+    const sunrealtype dTout = SUN_RCONST(del_t);
 
     // Number of output times
     const int Nt_p = static_cast<int>(ceil(Tf / dTout));
@@ -589,18 +511,9 @@ void TChem::Driver::doTimestep_sparse(const double del_t){
     sunrealtype tout = T0 + dTout;
 
     // Initial output
-    //
     real_type_2d_view_host_type y2d_h((y.HostView()).data(), udata.nbatches, udata.batchSize);
     sundials::kokkos::CopyFromDevice(y);
     Kokkos::fence();
-//    const auto density_host =
-//    Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace() , density);
-    const auto temperature_host =
-    Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace() , temperature);
-    const auto pressure_host =
-    Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), pressure);
-    const auto const_tracers_host =
-    Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(),const_tracers);
 
   // Time stepping
     int iout = 0;
@@ -621,7 +534,6 @@ void TChem::Driver::doTimestep_sparse(const double del_t){
 
   // Copy results back
   int i = 0;
-//  real_type_2d_view state("StateVector Devices", _nBatch, stateVecDim);
   for (ordinal_type j=0;j<n_active_gas_species;++j){
     _state(i,j+3) = y2d_h(i, j);
   }
@@ -631,32 +543,8 @@ void TChem::Driver::doTimestep_sparse(const double del_t){
     _state(i,j+3+_kmcd_host.nConstSpec) = y2d_h(i,j);
   }
 
-    long int nst, nfe, nsetups, nje, nni, ncfn, netf;
 
-    retval = CVodeGetNumSteps(cvode_mem, &nst);
-    check_flag(retval, "CVodeGetNumSteps");
-    retval = CVodeGetNumRhsEvals(cvode_mem, &nfe);
-    check_flag(retval, "CVodeGetNumRhsEvals");
-    retval = CVodeGetNumLinSolvSetups(cvode_mem, &nsetups);
-    check_flag(retval, "CVodeGetNumLinSolvSetups");
-    retval = CVodeGetNumErrTestFails(cvode_mem, &netf);
-    check_flag(retval, "CVodeGetNumErrTestFails");
-    retval = CVodeGetNumNonlinSolvIters(cvode_mem, &nni);
-    check_flag(retval, "CVodeGetNumNonlinSolvIters");
-    retval = CVodeGetNumNonlinSolvConvFails(cvode_mem, &ncfn);
-    check_flag(retval, "CVodeGetNumNonlinSolvConvFails");
-    retval = CVodeGetNumJacEvals(cvode_mem, &nje);
-    check_flag(retval, "CVodeGetNumJacEvals");
-
-    std::cout << "\nFinal Statistics:\n"
-              << "  Steps            = " << nst << "\n"
-              << "  RHS evals        = " << nfe << "\n"
-              << "  LS setups        = " << nsetups << "\n"
-              << "  Jac evals        = " << nje << "\n"
-              << "  NLS iters        = " << nni << "\n"
-              << "  NLS fails        = " << ncfn << "\n"
-              << "  Error test fails = " << netf << "\n";
-
+   print_cvode_statistics(cvode_mem);
 
    CVodeFree(&cvode_mem);
 

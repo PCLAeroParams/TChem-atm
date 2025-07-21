c7c10fac3ced8006695272d557daadb6002039d8
Sam Frederick <sf20@illinois.edu>
2025-07-21 12:32:23 -0500

    Add path to kokkos tools MemoryEvents tool

--- scripts/aerosol_performance/loadGPU.sh
+++ scripts/aerosol_performance/loadGPU.sh
@@ -1,6 +1,10 @@
 
 # Uncomment to use the Kokkos tools NVTX connector when profiling
 #export KOKKOS_TOOLS_LIBS=${KOKKOS_TOOLS_INSTALL_DIR}/lib64/libkp_nvtx_connector.so
+
+# MemoryEvents - tracks timeline of alloc and dealloc events and mem usage
+export KOKKOS_TOOLS_LIBS=${KOKKOS_TOOLS_INSTALL_DIR}/lib64/libkp_memory_events.so
+
 TCHEM_INSTALL_PATH=${TCHEM_DEVICE_INSTALL_PATH}
 exec=${TCHEM_INSTALL_PATH}/examples/${experiment_exe_root}.x
 machine_name=nid

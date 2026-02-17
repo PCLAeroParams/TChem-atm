50f23bb71e13cec6576d32a3e7dbc9b3559eebd9
zdaq12 <daquino2@illinois.edu>
2026-02-16 20:43:09 -0600

    add implementation and test

--- src/core/impl/TChem_Impl_MOSAIC.hpp
+++ src/core/impl/TChem_Impl_MOSAIC.hpp
@@ -4276,6 +4276,16 @@ struct MOSAIC{
     store(mosaic.ino3_a) = max(0.0, store(mosaic.ino3_a));
   } // form_nano3
 
+  KOKKOS_INLINE_FUNCTION static
+  void form_hcl(const MosaicModelData<DeviceType>& mosaic,
+                const real_type_1d_view_type& electrolyte,
+                const real_type_1d_view_type& store) {
+
+    electrolyte(mosaic.jhcl) = max(0.0, store(mosaic.icl_a));
+
+    store(mosaic.icl_a) = 0.0;
+  } // form_hcl
+
 };
 
 } // namespace Impl

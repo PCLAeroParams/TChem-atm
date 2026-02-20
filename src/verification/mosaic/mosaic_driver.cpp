ef587de110ab2a9275551c8fce3bff2c09c9f2a8
Zach D'Aquino <107204919+zdaq12@users.noreply.github.com>
2026-02-20 08:18:19 -0500

    add implementation and test for MOSAIC subroutine degas_hno3 (#147)
    * add verification data
    
    * fix data
    
    * add implementation and test

--- src/verification/mosaic/mosaic_driver.cpp
+++ src/verification/mosaic/mosaic_driver.cpp
@@ -101,6 +101,8 @@ void form_namsa(Ensemble *ensemble);
 
 void form_nano3(Ensemble *ensemble);
 
+void degas_hno3(Ensemble *ensemble);
+
 void form_hcl(Ensemble *ensemble);
 
 void form_hno3(Ensemble *ensemble);
@@ -206,6 +208,8 @@ int main(int argc, char **argv) {
       form_namsa(ensemble);
     } else if (func_name == "form_nano3") {
       form_nano3(ensemble);
+    } else if (func_name == "degas_hno3") {
+      degas_hno3(ensemble);
     } else if (func_name == "form_hcl") {
       form_hcl(ensemble);
     } else if (func_name == "form_hno3") {
